f2ac752578b2d4742c7e326ea30d4650c8673638
Jay Roberts <jayroberts91@gmail.com>
2018-08-13 13:43:09 -0700

    Added adas_utils module.
    - merge_annotations.py takes individual annotations in adas file
      and makes one large annotations file formatted like COCO annotation
      file.
    
    - made dataset_tools/create_adas_tf_record.py.
    
    - make_adas_tfrecords.bash makes tfrecords for adas data.

---
+++ research/object_detection/adas_utils/merge_annotations.py
@@ -0,0 +1,86 @@
+import json 
+import os 
+import argparse
+
+def merge_annotations(anno_dir,catids,out_name, 
+                        verbose = 0, keep_blank_annotations = False):
+    num_images = len(os.listdir(anno_dir))
+
+    images_data = [0]*num_images
+    annos_data = [0]*num_images
+    blank_annos = []
+    # Add the images and annotations
+
+    for i, anno_file in enumerate(os.listdir(anno_dir)):
+        path = anno_dir+anno_file
+        if i % 100 == 0 and verbose > 0:
+            msg = "Processing annotation file %d/%d"%(i,num_images)
+            print(msg)
+        with open(path,'r') as f:
+            data = json.load(f)
+            img_data = data['image']
+            
+            # Take only the base name of file_name
+            img_data['file_name'] = os.path.basename(img_data['file_name'])
+            
+            if len(data['annotation']) == 0:
+                an_data = data['annotation']
+                blank_annos.append(i)
+            else:
+                an_data = data['annotation']
+
+            images_data[i] = img_data
+            annos_data[i] = an_data
+    
+    if not keep_blank_annotations:
+        images_data = [images_data[k] for k in range(num_images) if not k in blank_annos ]
+        annos_data = [annos_data[k] for k in range(num_images) if not k in blank_annos ]
+    
+    # annos_data: list of list of dict -> list of dict
+    annos_data = [item for sublist in annos_data for item in sublist]
+    with open(catids, 'r') as fcat:
+        cats = json.load(fcat)
+
+    merged_annotations = {'images': images_data, 'annotations': annos_data, 'categories': cats}
+
+    with open(out_name, 'w') as fw:
+        json.dump(merged_annotations,fw, indent=1)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--anno-dir',
+                        help = 'Directory containing annotation json files.',
+                        type = str,
+                        required= True)
+    
+    parser.add_argument('--catids',
+                        help='The json file containg the category id to name map.',
+                        type = str,
+                        required = True)
+
+    parser.add_argument('--out-name',
+                        help='The full path including extension to save the merged json file.',
+                        default = 'merged_annotations.json')
+    
+    parser.add_argument('--verbose',
+                        help='Set to 1 if you want to see progress',
+                        default = 0,
+                        type = int)
+    parser.add_argument('--keep-blanks',
+                        help='Whether to keep data from images without annotations',
+                        default = False,
+                        type = bool)
+    args = parser.parse_args()
+    catids = args.catids
+    anno_dir = args.anno_dir
+    out_name = args.out_name
+    verbose = args.verbose
+    keep_em = args.keep_blanks
+
+    merge_annotations(anno_dir = anno_dir,
+                         catids = catids, 
+                         out_name = out_name, 
+                         verbose= verbose,
+                         keep_blank_annotations=keep_em)
