f596036417c304b3a07bbd2983041b671245385f
Jay Roberts <jayroberts91@gmail.com>
2018-08-24 14:16:45 -0700

    added model dir check

--- research/nonlocal_runtrain.bash
+++ research/nonlocal_runtrain.bash
@@ -17,6 +17,12 @@ MODEL_DIR=inception_v2_coco
 NUM_TRAIN_STEPS=10
 NUM_EVAL_STEPS=2
 
+if [ -d "$MODEL_DIR" ]; then
+  echo "Model directory exists"
+  echo "THIS MAY CAUSE LOADING ISSUES AND STOP THE MODEL FROM TRAINING"
+fi
+
+
 CUDA_VISIBLE_DEVICES=$cnd_gpus python object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
@@ -29,7 +35,6 @@ echo "Freezing the trained graph"
 echo "==========================="
 
 # From tensorflow/models/research/
-
 INPUT_TYPE=image_tensor
 PIPELINE_CONFIG_PATH=$MODEL_DIR/pipeline.config
 TRAINED_CKPT_PREFIX=$MODEL_DIR/model.ckpt-$NUM_TRAIN_STEPS
