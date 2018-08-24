59937d2caca67f01fd9bd380347b12f6d28dd3e1
Jay Roberts <jayroberts91@gmail.com>
2018-08-24 14:19:22 -0700

    initial commit. used to start object detection api on servers

---
+++ research/nonlocal_setup.bash
@@ -0,0 +1,5 @@
+source ~/tensorflow/bin/activate
+protoc object_detection/protos/*.proto --python_out=.
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+python object_detection/builders/model_builder_test.py
+
