879c0a9f9ba290f0d0d5416cc081181b98afc01d
Jay Roberts <jayroberts91@gmail.com>
2018-08-31 10:05:40 -0700

    Verbose prints every 100 instead of 3 annotations.

--- research/object_detection/adas_utils/make_adas_tfrecords.bash
+++ research/object_detection/adas_utils/make_adas_tfrecords.bash
@@ -2,19 +2,43 @@
 # ADAS data. Assumes all images are 
 # in jpeg compression format
 
+# Should be run from tensorflow-models/research/object_detection/adas_utils/
 
-IMAGE_DIR_BASE=/home/jroberts/conservator_datasets/ADAS/merged
+DIR_BASE=$1       # Should not have trailing /
+OUTPUT_DIR=$2     #Where to save the tfrecords
 
-TRAIN_IMAGE_DIR=$IMAGE_DIR_BASE/train/PreviewData     # Where the train images are 
-VAL_IMAGE_DIR=$IMAGE_DIR_BASE/val/PreviewData         # Where the val images are
+# Some of the directores put things in 
+#           PreviewData/
+# Some put it in
+#           Data/ 
 
-TRAIN_ANNOTATIONS_FILE=$IMAGE_DIR_BASE/train/merged_annotations.json    # A merged annoations file as outputed by merge_annoations.py
-VAL_ANNOTATIONS_FILE=$IMAGE_DIR_BASE/val/merged_annotations.json        # A merged annoations file as outputed by merge_annoations.py
 
-OUTPUT_DIR=/home/jroberts/conservator_datasets/ADAS/merged/trecords     # Where to save the tfrecords
+TRAIN_IMAGE_DIR=$DIR_BASE/train/PreviewData     
+VAL_IMAGE_DIR=$DIR_BASE/val/PreviewData
+
+TRAIN_ANNOTATIONS_DIR=$DIR_BASE/train/Annotations/
+VAL_ANNOTATIONS_DIR=$DIR_BASE/val/Annotations/
+
+echo "Merging train annotations"
+python merge_annotations.py --anno-dir $TRAIN_ANNOTATIONS_DIR \
+                              --catids $DIR_BASE/catids.json \
+                              --out-name $DIR_BASE/train/merged_annotations.json \
+                              --verbose 1
+
+echo "Merging val annotations"
+python merge_annotations.py --anno-dir $VAL_ANNOTATIONS_DIR \
+                              --catids $DIR_BASE/catids.json \
+                              --out-name $DIR_BASE/val/merged_annotations.json \
+                              --verbose 1
+                              
+# Make the merged annotations files
+# to be compatible with MSCOCO format
+TRAIN_ANNOTATIONS_FILE=$DIR_BASE/train/merged_annotations.json    
+VAL_ANNOTATIONS_FILE=$DIR_BASE/val/merged_annotations.json        
 
 TRAIN_SHARDS=2    # Sharding a dataset can help throughput during training (Or so TF says....)
 
+echo "Creating tfrecords"
 python ../dataset_tools/create_adas_tf_record.py --logtostderr \
       --train_image_dir=$TRAIN_IMAGE_DIR \
       --val_image_dir=$VAL_IMAGE_DIR \
